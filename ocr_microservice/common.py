b53f04b35ef08f1f6c48c037eba7569f5cfa371f
Joseph Catrambone <jo.jcat@gmail.com>
2020-08-04 13:59:20 -0700

    Text template search is working.  Need to test on real document.s

--- ocr_microservice/common.py
+++ ocr_microservice/common.py
@@ -1,7 +1,7 @@
 import numpy
 import os
 import cv2
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from pdfminer.high_level import extract_text
 import pytesseract
@@ -109,18 +109,31 @@ def extract_text_from_image(infile, filepath):
 
 def _edge_detect(img):
     # A simple wrapper for edge detection that sums the DX/DY components.
-    if img.shape[-1] > 1:  # Force grey if it's not already.
+    if len(img.shape) > 2 and img.shape[-1] > 1:  # Force grey if it's not already.
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges_x = numpy.zeros_like(img)
-    edges_y = numpy.zeros_like(img)
-    cv2.Sobel(img, cv2.CV_64F, 1, 0, dst=edges_x, ksize=3)
-    cv2.Sobel(img, cv2.CV_64F, 0, 1, dst=edges_y, ksize=3)
-    return numpy.abs(edges_x) + numpy.abs(edges_y)
+    # DO NOT LISTEN TO THE DOCS!  Specifying dst != None will return empties.
+    edges_x = cv2.Sobel(img.copy(), cv2.CV_32F, 1, 0, ksize=1)
+    edges_y = cv2.Sobel(img.copy(), cv2.CV_32F, 0, 1, ksize=1)
+    return (numpy.abs(edges_x) + numpy.abs(edges_y)) * 0.5
 
 
-def match_template_in_image(document_filepath, template_filepath, threshold=0.0):
-    doc_image = cv2.imread(document_filepath)
-    template_image = cv2.imread(template_filepath)
+def match_template_in_image(
+        doc_image=None, template_image=None,
+        document_filepath: str = None, template_filepath: str = None,
+        threshold: float = 0.7,
+        max_downsampling: int = 4
+):
+    """
+    Given a document and template (or doc filename and template filename),
+    find all the template matches above the threshold in the image across all scales.
+    Returns an ARRAY with {"bounding_box":[x,y,w,h], "confidence":##.##}.
+    """
+    assert (doc_image is not None) or (document_filepath is not None)
+    assert (template_image is not None) or (template_filepath is not None)
+    if document_filepath:
+        doc_image = cv2.imread(document_filepath)
+    if template_filepath:
+        template_image = cv2.imread(template_filepath)
 
     # Make greyscale explicitly, though many of our docs will be grey anyway.
     doc_image = cv2.cvtColor(doc_image, cv2.COLOR_BGR2GRAY)
@@ -137,16 +150,21 @@ def match_template_in_image(document_filepath, template_filepath, threshold=0.0)
     matches = list()
     scale = 1
     # When our doc_image is less than the template size on any axis, we know it can't match.
-    while doc_image.shape[0]//scale > template_image.shape[0] and doc_image.shape[1]//scale > template_image.shape[1]:
+    while doc_image.shape[0]//scale > template_image.shape[0] \
+            and doc_image.shape[1]//scale > template_image.shape[1] \
+            and scale < max_downsampling:
         # Yes, PIL is easier to use for resizing, but then we have to convert back and forth.
         search_image = numpy.zeros((doc_image.shape[0]//scale, doc_image.shape[1]//scale, 1), dtype=numpy.uint8)
-        cv2.resize(doc_image, dst=search_image, fx=1.0/float(scale), fy=1.0/float(scale))
+        cv2.resize(doc_image, dsize=None, dst=search_image, fx=1.0/float(scale), fy=1.0/float(scale))
 
         # Compute the edges _NOW_ rather than before reduction so we actually get fovation.
         search_edges = _edge_detect(search_image)
 
         # Template match, resize our response, and accumulate it.
         scaled_match_response = cv2.matchTemplate(search_edges, template_edges, cv2.TM_CCORR_NORMED)
+
+        # DEBUG: Show template:
+        #Image.fromarray(numpy.clip(255 * scaled_match_response[:, :], 0, 255).astype(numpy.uint8)).show()
 
         # Go over the scaled match response and add the bounding boxes if they're above threshold.
         # Don't forget to resize the bounding boxes by the inverse of the scale factor.
@@ -161,15 +179,59 @@ def match_template_in_image(document_filepath, template_filepath, threshold=0.0)
         if max_response > threshold:
             matches.append({
                 "bounding_box": [
-                    max_response_position[0]*scale, # x
-                    max_response_position[1]*scale, # y
-                    template_edges.shape[0]*scale, # width
-                    template_edges.shape[1]*scale, # height
+                    # NOTE THE COORDINATE CHANGE!
+                    # OpenCV uses column-major because we can't have nice things.
+                    max_response_position[1]*scale,  # x
+                    max_response_position[0]*scale,  # y
+                    template_edges.shape[1]*scale,  # width
+                    template_edges.shape[0]*scale,  # height
                 ],
-                "confidence": max_response
+                "confidence": max_response,
+                "scale": scale,
             })
 
         # Don't forget to update the scale.
         scale += 1
 
     return matches
+
+
+def match_text_in_image(
+        text: str,
+        document_filename: str = None,
+        doc_image=None,
+        threshold: float = 0.0,
+        text_size: int = 12,
+        font: str = "Arial.ttf"
+):
+    # Assuming 96 DPI, 6pt = 8px.  12pt = 16px.
+    # Allocate a drawing area fitting of the text size.
+    bg_color = (0, 0, 0)
+    fg_color = (255, 255, 255)
+    template_size = (int(len(text)*text_size), int((1+text.count('\n'))*text_size*1.5))
+
+    template = Image.new("RGB", template_size, bg_color)
+    fnt = ImageFont.truetype(font, text_size)
+    ctx = ImageDraw.Draw(template)
+
+    # Draw to the template context
+    ctx.multiline_text((0, 0), text, font=fnt, fill=fg_color)
+
+    # Convert the template to a cv2 image (which is basically a numpy array).
+    template = numpy.asarray(template, dtype=numpy.uint8)
+
+    return match_template_in_image(
+        document_filepath=document_filename,
+        doc_image=doc_image,
+        template_image=template,
+        threshold=threshold
+    )
+
+def _debug_highlight_regions(doc_filename, matches):
+    img = Image.open(doc_filename)
+    ctx = ImageDraw.Draw(img)
+    for m in matches:
+        x, y, w, h = m['bounding_box']
+        ctx.rectangle([x, y, x+w, y+h], fill=None, outline="red")
+    print(matches)
+    img.show()
